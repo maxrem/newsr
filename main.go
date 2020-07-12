7feef9ae3d3a6eeb1d6e043654057d5744aeb539
Max Remkes <max@pbwebmedia.com>
2020-07-12 11:02:05 +0200

    Added published time check function

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/mmcdole/gofeed"
 	"log"
+	"time"
 )
 
 var googleNewsUrl = "https://news.google.com/news?hl=en-US&ceid=US:en&sort=date&gl=US&num=10&output=rss&q="
@@ -21,6 +22,16 @@ func main() {
 	log.Println(feed.Len())
 	for i := 0; i < feed.Len(); i++ {
 		item := feed.Items[i]
-		log.Println(item.Title)
+
+		timeParsed, err := time.Parse("Mon, 02 Jan 2006 15:04:05 MST", item.Published)
+		if err != nil {
+			log.Println(err)
+		}
+
+		timeSince := time.Since(timeParsed)
+
+		if timeSince < 48 * time.Hour {
+			log.Println(item.Title, timeParsed)
+		}
 	}
 }
